2f10fba0ffb566209997bdca52ff8f0a43fbf51f
Alex Redding <Alexredding180@gmail.com>
2020-10-14 21:53:10 +0100

    Fixes

--- Dns/Clients/Dynv6Client.cs
+++ Dns/Clients/Dynv6Client.cs
@@ -10,13 +10,15 @@ namespace TCAdminSubdomain.Dns.Clients
         public readonly RestClient RestClient;
         private const string BaseUrl = "https://dynv6.com/api/v2";
         private readonly DnsProviderType _dnsProviderType = new DnsProviderType(2);
-        private readonly Dynv6Configuration _configuration;
 
         public Dynv6Client()
         {
             RestClient = new RestClient(BaseUrl);
-            _configuration = _dnsProviderType.Configuration.Parse<Dynv6Configuration>();
-            RestClient.Authenticator = new JwtAuthenticator(_configuration.Token);
+            var configuration = _dnsProviderType.Configuration.Parse<Dynv6Configuration>();
+            if (!string.IsNullOrEmpty(configuration.Token))
+            {
+                RestClient.Authenticator = new JwtAuthenticator(configuration.Token);
+            }
         }
     }
 }
--- install.sql
+++ install.sql
@@ -13,13 +13,13 @@ INSERT INTO tc_site_map (page_id, module_id, parent_page_id, parent_page_module_
                          controller, action, display_name, page_small_icon, panelbar_icon, show_in_sidebar,
                          view_order, required_permissions, menu_required_permissions, page_manager,
                          page_search_provider, cache_name)
-VALUES (1070, 'ceb0b7e0-59f6-4290-991d-b766b30f1ff1', null, null, null, '', '', 'Subdomain', 'Create',
+VALUES (1, 'ceb0b7e0-59f6-4290-991d-b766b30f1ff1', null, null, null, '', '', 'Subdomain', 'Create',
         'Create Subdomain', 'MenuIcons/Base/Info24x24.png', null, 0, null, '', '', null, null, '');
 INSERT INTO tc_site_map (page_id, module_id, parent_page_id, parent_page_module_id, category_id, url, mvc_url,
                          controller, action, display_name, page_small_icon, panelbar_icon, show_in_sidebar,
                          view_order, required_permissions, menu_required_permissions, page_manager,
                          page_search_provider, cache_name)
-VALUES (1071, 'ceb0b7e0-59f6-4290-991d-b766b30f1ff1', 40, '07405876-e8c2-4b24-a774-4ef57f596384', 1,
+VALUES (2, 'ceb0b7e0-59f6-4290-991d-b766b30f1ff1', 40, '07405876-e8c2-4b24-a774-4ef57f596384', 1,
         '/Subdomain/Configure', '/Subdomain/Configure', 'Subdomain', 'Configure', 'Configure Subdomain Providers',
         'MenuIcons/Base/ServerComponents24x24.png', 'MenuIcons/Base/ServerComponents16x16.png', 1, 1071,
         '({07405876-e8c2-4b24-a774-4ef57f596384,0,8})', '({07405876-e8c2-4b24-a774-4ef57f596384,0,8})', null, null, '');
